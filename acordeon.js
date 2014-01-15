a0a84e1d71ee863e2a9e668985d70fa5457acabb
Yadira Cisneros <ycisne@gmail.com>
2014-01-15 10:02:22 -0600

    Acordeón
    Primera versión del acordeón

--- acordeon.js
+++ acordeon.js
@@ -1,4 +1,5 @@
 /*
+ *
  *	acordeon - jQueryPlugin
  *	version: 1.0 (Wed, 14 Jan 2014)
  *	@requires jQuery v1.8.3 or later
@@ -7,6 +8,7 @@
  *	Licence:
  *
  *	Copyright 2014 Yadira Cisneros - ycisne@gmail.com
+ *
  */
 	(function($){
 		var methods = {
